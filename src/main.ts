2d2b6d4ba255ee40d1f4a1011f9f9f830353c45e
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-07-18 19:16:45 +0530

    Setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors();
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Easy Payslip API')
+    .setDescription('The Easy Payslip API lets you manage your payslips and employees')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
